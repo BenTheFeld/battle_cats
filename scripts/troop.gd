13fa01e305978bbbc3d6edcd42e7989b6be6d1c3
BenTheFeld <benjamin.feldgendler@gmail.com>
2024-12-19 23:35:30 +0100

    Creates range hitbox script(troop_range.gd) and ties it in with basic_troop.gd

--- scripts/troop.gd
+++ scripts/troop.gd
@@ -2,14 +2,10 @@ extends Area2D
 
 var speed = -1
 
-
-func _on_area_entered(area):
-	if area.is_in_group("enemy"):
-		speed = 0
-		print("enemy!")
-	else: 
-		speed = -1
-
-		
+func set_speed(val_a):
+	speed = val_a
+	print(speed)
+	
+	
 func _physics_process(float):
 	move_local_x(speed)
