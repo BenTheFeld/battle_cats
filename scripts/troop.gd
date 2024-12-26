8edb3851e55a1bf92df60c3a8648976b021e8329
Benjamin Feldgendler <befea005@osloskolen.no>
2024-12-26 16:02:20 +0100

    Fixes animation player bug and makes the cooldown timer work

--- scripts/troop.gd
+++ scripts/troop.gd
@@ -2,28 +2,32 @@ extends Area2D
 
 var speed = -1
 var is_ready: bool = true
+var enemy = 0
 
 @onready var troop_sprite: AnimatedSprite2D = $troop_sprite
-
+	
 func attack():
-	if  is_ready == true:
+	if  is_ready == true and enemy > 0:
 		is_ready = false
 		troop_sprite.play("attack")
 		$troop_sprite/cooldown.start()
-		troop_sprite.play("wait")
-
+			
 func _on_timer_timeout() -> void:
 	is_ready = true
 	
+func _process(delta: float) -> void:
+	attack()
+	
 func _on_troop_range_area_entered(area: Area2D) -> void:
 	if  area.is_in_group("enemy"):
+		enemy += 1
 		speed = 0
-		attack()
+		print(enemy)
 		
 func _on_troop_range_area_exited(area: Area2D) -> void:
 	if  area.is_in_group("enemy"):
+		enemy -= 1
 		speed = -1
-		print("no enemy")
 		troop_sprite.play("walk")
 
 func _physics_process(delta: float) -> void:
