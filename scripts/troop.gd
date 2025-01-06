b446d4225d4171a8c6d9884144c3a73bea321c52
Benjamin Feldgendler <befea005@osloskolen.no>
2025-01-06 15:38:30 +0100

    Switches to animationplayer for troop animations adding walk and attack animations

--- scripts/troop.gd
+++ scripts/troop.gd
@@ -4,16 +4,20 @@ var speed = -1
 var is_ready: bool = true
 var enemy = 0
 
-@onready var troop_sprite: AnimatedSprite2D = $troop_sprite
-	
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
+
+#@onready var troop_sprite: AnimatedSprite2D = $troop_sprite
+
+#@onready var cooldown: Timer = $troop_sprite/cooldown
+
 func attack():
 	if  is_ready == true and enemy > 0:
 		is_ready = false
-		troop_sprite.play("attack")
-		$troop_sprite/cooldown.start()
-			
-func _on_timer_timeout() -> void:
-	is_ready = true
+		animation_player.play("attack")
+		#$troop_sprite/cooldown.start()
+		#if troop_sprite.get_frame() == 2:
+		#	print("1dmg")
+		#print(troop_sprite.get_frame())
 	
 func _process(delta: float) -> void:
 	attack()
@@ -28,7 +32,7 @@ func _on_troop_range_area_exited(area: Area2D) -> void:
 	if  area.is_in_group("enemy"):
 		enemy -= 1
 		speed = -1
-		troop_sprite.play("walk")
+		animation_player.play("walk")
 
 func _physics_process(delta: float) -> void:
 	move_local_x(speed)
